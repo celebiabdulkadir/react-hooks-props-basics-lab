a0cb02a3bb2fe8a48d23acb8fd10acdc5057b1fc
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-06-07 00:39:54 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import Links from './Links';
+import user from '../data/user';
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
+
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home name={user.name} color={user.color} city={user.city} />
+			<About bio={user.bio} links={user.links} />
+			<Links />
+		</div>
+	);
 }
 
 export default App;
